16bc340bed28e9b2e220713f49ff59d9356192a1
Jacob Probst <jacob@probst.pw>
2022-09-29 17:10:45 -0500

    adding foundations of mutation tracking

--- data/mutsheets.js
+++ data/mutsheets.js
@@ -44,7 +44,7 @@ const sendUpdate = () => {
     let sheets = google.sheets({ version: "v4", auth: auth })
     sheets.spreadsheets.values.update({
         spreadsheetId: spreadsheetId,
-        range: `${spreadsheetTabNameRoster}!A:J`,
+        range: `${spreadsheetTabNameMutations}!A:M`,
         valueInputOption: 'USER_ENTERED',
         resource: {
             values: buildValueArray(queuedObj)
@@ -58,13 +58,13 @@ const sendUpdate = () => {
 }
 
 const buildValueArray = (obj) => {
-    let value = [["Discord ID", "Name", "Company", "Level", "Gearscore", "Weapon 1", "Weapon 2", "Heartrune", "Weight", "Notes"]]
+    let value = [["Discord ID", "Name", "Faction", "Company", "Level", "Gearscore", "Role", "Weapon 1", "Weapon 2", "Heartrune", "Weight", "War", "Notes"]]
 
     let entries = Object.entries(obj).sort((a, b) => {
         return a[1].name.localeCompare(b[1].name);
     })
     for (const [k, v] of entries) {
-        value.push([k, v.name, v.company || " ", v.level || " ", v.gearscore || " ", v.primaryWeapon || " ", v.secondaryWeapon || " ", v.heartrune || " ", v.weight || " ", v.notes || " "])
+        value.push([k, v.name, v.faction || " ", v.company || " ", v.level || " ", v.gearscore || " ", v.role || " ", v.primaryWeapon || " ", v.secondaryWeapon || " ", v.heartrune || " ", v.weight || " ", v.warInterest || " ", v.notes || " "])
     }
     return value
 }
